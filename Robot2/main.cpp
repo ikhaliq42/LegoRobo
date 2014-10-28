936acdac815657e66431691d000d9df41f014ef3
Imran Khaliq <s1461613@brindisi.inf.ed.ac.uk>
2014-10-28 22:15:02 +0000

    Object recognition code added

--- Robot2/main.cpp
+++ Robot2/main.cpp
@@ -13,8 +13,13 @@
 #include "Globals.h"
 #include <iostream>
 #include <vector>
+#include <opencv2/opencv.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/nonfree/features2d.hpp>
+#include <opencv2/features2d/features2d.hpp>
 
 using namespace std;
+using namespace cv;
 
 int square = 0;
 
@@ -117,7 +122,7 @@ void gotobase(Robot *robo) {
     robo->tooCloseResponse(0);
     int p = (int) robo->poses.size() - 1 - q;
     //while(abs(robo->xa[q + p + 2] - robo->xa[q - p]) < 40 || abs(robo->ya[q+p +2] - robo->ya[q-p]) < 40){
-    double jim = pow(robo->xa[robo->poses.size() - 1] - robo->xa[robo->poses.size() - 1 - p],2) abs(robo->ya[robo->poses.size() - 1] - robo->ya[robo->poses.size() - 1 - p])
+    //double jim = pow(robo->xa[robo->poses.size() - 1] - robo->xa[robo->poses.size() - 1 - p],2) abs(robo->ya[robo->poses.size() - 1] - robo->ya[robo->poses.size() - 1 - p])
     while(abs(robo->xa[robo->poses.size() - 1] - robo->xa[robo->poses.size() - 1 - p]) < 60 && abs(robo->ya[robo->poses.size() - 1] - robo->ya[robo->poses.size() - 1 - p]) < 60){
           
         robo->DriveMotorsStraight(0, 100);
@@ -571,13 +576,126 @@ void Rotate() {
 
 }
 
+
+
+
+
+int SurfDemo(Robot *robo) {
+    
+        // Program to illustrate SURF keypoint and descriptor extraction, and matching using FLANN
+        // Author: Samarth Manoj Brahmbhatt, University of Pennsylvania
+
+	Mat train = imread("chewbacca.png"), train_g;
+	cvtColor(train, train_g, CV_BGR2GRAY);
+	int matchThreshold = 5;
+
+	//detect SIFT keypoints and extract descriptors in the train image
+	vector<KeyPoint> train_kp;
+	Mat train_desc;
+	SurfFeatureDetector featureDetector(100);
+	featureDetector.detect(train_g, train_kp);
+	SurfDescriptorExtractor featureExtractor;
+	featureExtractor.compute(train_g, train_kp, train_desc);
+
+	// FLANN based descriptor matcher object
+	FlannBasedMatcher matcher;
+	vector<Mat> train_desc_collection(1, train_desc);
+	matcher.add(train_desc_collection);
+	matcher.train();
+
+	// VideoCapture object
+	VideoCapture cap(0);
+	unsigned int frame_count = 0;
+        
+	while(char(waitKey(1)) != 'q') {
+
+		double t0 = getTickCount();
+		Mat test, test_g;
+		Mat fred;
+		cap >> fred;
+		cap >> fred;
+		cap >> fred;
+		cap >> fred;
+		cap >> fred;
+		cap >> fred;
+		cap >> test;
+		if(test.empty())continue;
+		cvtColor(test, test_g, CV_BGR2GRAY);
+
+		//detect SIFT keypoints and extract descriptors in the test image
+		vector<KeyPoint> test_kp;
+		Mat test_desc;
+		featureDetector.detect(test_g, test_kp);
+		featureExtractor.compute(test_g, test_kp, test_desc);
+
+		// match train and test descriptors, getting 2 nearest neighbors for all test descriptors
+		vector<vector<DMatch> > matches;
+		matcher.knnMatch(test_desc, matches, 2);
+
+		// filter for good matches according to Lowe's algorithm
+		vector<DMatch> good_matches;
+		for(int i = 0; i < matches.size(); i++) {
+			if(matches[i][0].distance < 0.6 * matches[i][1].distance) good_matches.push_back(matches[i][0]);
+		}
+
+		
+		//Indicate with movement if there is a match
+		if (good_matches.size() >= matchThreshold) {
+			robo->Turn(0,3,defaultPower);
+			robo->Turn(1,3,defaultPower);
+			robo->Turn(0,3,defaultPower);
+			robo->Turn(1,3,defaultPower);
+		}
+
+		Mat img_show;
+		drawMatches(test, test_kp, train, train_kp, good_matches, img_show);
+		imshow("Matches", img_show);
+		cout << "Frame rate = " << getTickFrequency() / (getTickCount() - t0) << endl;
+	}
+
+	return 0;
+}
+
+int ObjectRecognition() {
+    
+    Robot *robo = new Robot;
+
+    //robo->InitialiseMotors();
+    robo->initialiseInterfacekit();
+    //srand((unsigned) time(&t));
+
+    robo->startFlag = false;
+    robo->exitFlag = false; // just in case
+    
+    printf("\nStart flag = %d\n", robo->startFlag);	
+
+    // wait for button press on robot
+    while (robo->startFlag == false) {}       
+    
+    SurfDemo(robo);
+    
+    robo->ShutDownInterfacekit();
+    robo->ShutDownMotors();
+    robo->~Robot();
+
+
+    return 0;
+    
+}   
+
+
 int main(int argc, char** argv) {
 
-    MoveAndAvoid();
+    //MoveAndAvoid();
     //MoveForward();
     // FaceWall();
     //MoveSquare();
     //Rotate();
+    ObjectRecognition();
+    //SurfDemo();
     return 0;
 }
+
+
+
 
