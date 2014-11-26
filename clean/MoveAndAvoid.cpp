941b407145ac908ca348b89ca5560926c022cbdf
Peter Stefanov <g.i.joe_soldier@abv.bg>
2014-11-26 15:08:09 +0000

    Robot singleton wrapper added!

--- clean/MoveAndAvoid.cpp
+++ clean/MoveAndAvoid.cpp
@@ -1,50 +1,50 @@
 #include <iostream>
 #include <stdio.h>
 #include <unistd.h>
+#include <boost/smart_ptr/shared_ptr.hpp>
 
 #include "SensorManager.h"
 #include "MotorManager.h"
+#include "Robot.h"
 
 int main(int rgc, char* argv[]){
 	std::cout << "Program commences!" << std::endl;
 
-	SensorManager sensors;
-	MotorManager motors;
+	//SensorManager sensors;
+	//MotorManager motors;
+	Robot_ptr robot = Robot::getInstance();
 	std::cout << "Go (test sensors and motors)" << std::endl;
 	sleep(2);
 
-	motors.Go(MotorManager::Forward, 100.0);
+	robot->motors.Go(MotorManager::Forward, 100.0);
 	std::cout << "===================================" << std::endl;
 	std::cout << "Let's go forward at speed 100 :)" << std::endl;
 	std::cout << "===================================" << std::endl;
 	sleep(5);
 
-	motors.Go(MotorManager::Backward, 100.0);
+	robot->motors.Go(MotorManager::Backward, 100.0);
 	std::cout << "===================================" << std::endl;
 	std::cout << "Let's go backward at speed 100 :)" << std::endl;
 	std::cout << "===================================" << std::endl;
 	sleep(5);
 
-	motors.Go(MotorManager::Left, 100.0);
+	robot->motors.Go(MotorManager::Left, 100.0);
 	std::cout << "===================================" << std::endl;
 	std::cout << "Let's go left at speed 100 :)" << std::endl;
 	std::cout << "===================================" << std::endl;
 	sleep(5);
 
-	motors.Go(MotorManager::Right, 100.0);
+	robot->motors.Go(MotorManager::Right, 100.0);
 	std::cout << "===================================" << std::endl;
 	std::cout << "Let's go left at speed 100 :)" << std::endl;
 	std::cout << "===================================" << std::endl;
 	sleep(5);
 
-	motors.Stop();
+	robot->motors.Stop();
 	std::cout << "===================================" << std::endl;
 	std::cout << "Let's pause the motors :)" << std::endl;
 	std::cout << "===================================" << std::endl;
 
-
-	sensors.~SensorManager();
-	motors.~MotorManager();
 	std::cout << "Program terminates!" << std:: endl;
 	return 0;
 }
